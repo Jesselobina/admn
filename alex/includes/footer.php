5eb4a46969fa04b3e7f87acda9bd6ce03e945a97
Jesselobina <165880242+Jesselobina@users.noreply.github.com>
2025-09-30 22:43:57 +0800

    Delete alex directory

--- alex/includes/footer.php
+++
@@ -1,3 +0,0 @@
-<script src="<?php echo BASE_URL; ?>assets/script.js"></script>
-</body>
-</html>
--- alex/modules/asset/depreciation.php
+++
@@ -1,5 +0,0 @@
-<h2 class="text-xl font-bold text-gray-800 mb-6">Depreciation Calculator</h2>
-<div class="text-center text-gray-500 py-16">
-    <i class="fas fa-chart-line text-4xl text-gray-300 mb-4"></i>
-    <p>Tools for calculating asset depreciation will be provided here.</p>
-</div>
